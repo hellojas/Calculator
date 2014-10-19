773e7638e4a5ac2a10371bc068dc2048ef6a9b25
Jasmine Hsu <jasminechsu@gmail.com>
2014-10-19 12:24:46 -0400

    standard calculator iOS app
    calculator iOS

---
+++ Calculator/CalculatorViewController.h
@@ -0,0 +1,14 @@
+//
+//  CalculatorViewController.h
+//  Calculator
+//
+//  Created by Jasmine Hsu on 6/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *display;
+
+@end
